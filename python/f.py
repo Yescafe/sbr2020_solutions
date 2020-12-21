778f7718083d82af1ffb2cda7da979b61477f1d2
Ivan Chien <qyc027@gmail.com>
2020-12-21 10:53:09 +0800

    Fix Python solution of problem F

--- python/f.py
+++ python/f.py
@@ -1,14 +1,2 @@
-import math
 expr = input()
-if expr.find('+') != -1:
-    ns = expr.split('+')
-    if len(ns) == 3:
-        print(int(ns[1]) + int(ns[2]))
-    else:
-        print(int(ns[0]) + int(ns[1]))
-else:
-    ns = expr.split('-')
-    if len(ns) == 3:
-        print(abs(-int(ns[1]) - int(ns[2])))
-    else:
-        print(abs(int(ns[0]) - int(ns[1])))
+print(abs(eval(expr)))
